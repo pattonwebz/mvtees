3f06f977e0defe514d3bb0c0fad60d3615af237d
William Patton <will@pattonwebz.com>
2018-01-14 19:28:15 +0000

    Tweaks for code standards purposes

--- mvtees.js
+++ mvtees.js
@@ -4,33 +4,34 @@
  * ES6 class to hold the test library.
  */
 class MVTees {
-	// constructor to do instasiate tasks (this )
+	// constructor to do instasiate tasks (this).
 	constructor( name, debug ) {
-		// set
+		// set.
 		this.name = name || 'MVTees';
 		this.tests = [];
 
-		// debug toggle
+		// debug toggle.
 		this.debug = debug || false;
 
-		// this is only present in dev
+		// this is only present in dev.
 		this.dev_mock_tests();
 
 	}
 
 	/**
 	 * Functino to grab the tests (or a single test) from the object.
+	 *
 	 * @param  {mixed}  [test_id=false] an integer of a test if we want one.
 	 * @return {mixed}                  false if no tests, test item or array of them otherwise.
 	 */
 	get_tests( test_id = false ) {
-		// debug block
-		if( this.debug ){
+		// debug block.
+		if ( this.debug ) {
 			console.log( 'get_tests: testid=' + test_id );
 		}
 		// return either a single test object or multiple.
 		var result;
-		if( test_id || 0 === test_id ) {
+		if ( test_id || 0 === test_id ) {
 			result = this.tests[ test_id ];
 		} else {
 			result = this.tests;
@@ -49,8 +50,8 @@ class MVTees {
 			let value = storage.getItem( this.name + '-' + key );
 			return value;
 		} else {
-			// debug block
-			if( this.debug ){
+			// debug block.
+			if ( this.debug ) {
 				console.log( 'Error! Local Storage not available.' );
 			}
 		}
@@ -69,21 +70,22 @@ class MVTees {
 
 	run_tests(){
 		// WIP: this just loops and logs messages that have been saved to local storage.
-		for (let i = 0; i < this.tests.length; i++) {
+		let length = this.tests.length;
+		for (let i = 0; i < length; i++) {
 			let obj = this.get_local_storage( this.tests[i].name );
-		    console.log( obj );
+			console.log( obj );
 		}
 	}
 
 	dev_mock_tests() {
 		this.tests.push({
 			name: 'test1',
-		 	data: 'first'
+			data: 'first'
 		});
 		this.set_local_storage( this.tests[0].name, this.tests[0] )
 		this.tests.push({
 			name: 'test2',
-		 	data: 'second'
+			data: 'second'
 		});
 		this.set_local_storage( this.tests[1].name, this.tests[1] )
 	}
@@ -91,14 +93,15 @@ class MVTees {
 	/**
 	 * Generate a valid uuid for users.
 	 *
-	 * inpiration for this entire function was found at the link
+	 * Inspiration for this entire function was found at the link.
+	 *
 	 * @link https://gist.github.com/jed/982883
 	 *
 	 * @return {string} a valid uuidv4
 	 */
 	static generate_uuidv4() {
-		return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-			(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+		return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+			(c ^ crypto.getRandomValues( new Uint8Array( 1 ) )[0] & 15 >> c / 4).toString( 16 )
 		)
 	}
 
